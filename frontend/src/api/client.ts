f2323a0a7d61517ff18468368ed5a6e56fab0aae
ranjan781 <yranjan781@gmail.com>
2026-03-29 13:19:50 +0530

    Fix: production API URL

--- frontend/src/api/client.ts
+++ frontend/src/api/client.ts
@@ -1,18 +1,16 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: 'http://localhost:8000/api/v1',
+  baseURL: 'https://hireai-backend-fbxa.onrender.com/api/v1',
   headers: { 'Content-Type': 'application/json' }
 })
 
-// Har request mein token automatically add hoga
 api.interceptors.request.use((config) => {
   const token = localStorage.getItem('token')
   if (token) config.headers.Authorization = `Bearer ${token}`
   return config
 })
 
-// 401 pe logout
 api.interceptors.response.use(
   (res) => res,
   (err) => {
